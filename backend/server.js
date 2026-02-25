d19e92b4e338130c9d5f548caebefe2b697a809a
Adarsh Kumar <162141376+Adarshkumar0509@users.noreply.github.com>
2026-02-25 22:29:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,3 @@
-//  start server
-// require('dotenv').config();
-// const app = require('./src/app');
-// const connectDB = require('./src/db/db');
-
-// connectDB();
-
-// app.listen(3000, () => {
-//     console.log("Server is running on port 3000");
-// })
-
 // start server
 require('dotenv').config();
 const app = require('./src/app');
